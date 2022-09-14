0fe3f6a4b45acc250ddb7a8085fb748e2942c941
Sigurd Kjelsbøl Huse <sigurdkh1234@gmail.com>
2022-09-14 11:25:22 +0200

    Started work on problem 3

---
+++ Project_2/Problem3.cpp
@@ -0,0 +1,7 @@
+#include "functions.hpp"
+
+int main(){
+    arma::mat A(1,1);
+    int k, l;
+    max_offdiag_symmetric(A, k, l);
+}
---
+++ Project_2/functions.hpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+#include <armadillo>
+
+double max_offdiag_symmetric(arma::mat A, int& k, int &l){
+    return 0.0;
+}
