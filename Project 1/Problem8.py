f20c02a0fa20635e44137d07ac879783f3c2ed20
Odin107 <Odinjo00@gmail.com>
2022-09-14 09:33:45 +0200

    small changes to the plot

--- Project 1/Problem8.py
+++ Project 1/Problem8.py
@@ -7,16 +7,16 @@ for n in [10, 100, 1000, 10000]:
     exact = np.loadtxt(f"exact_solution_n_{n}.txt", delimiter = ", ", usecols = range(2)) 
     numeric = np.loadtxt(f"numeric_solution_n_{n}.txt", delimiter = ", ", usecols = range(2))
 
-    diff = np.log10(np.abs(exact[:,1] - numeric[:,1]))
+    diff = np.log10((exact[1:-1,1] - numeric[1:-1,1]))
 
-    plt.plot(exact[1:-1:,0], diff[1:-1], label = f"N = {n}")  
+    #plt.plot(exact[1:-1,0], diff, label = f"N = {n}")
 
 plt.legend()
 plt.grid()
 plt.title('Absolute error')
 plt.xlabel("x-values")
 plt.ylabel("log 10")
-plt.show()
+#plt.show()
 #plt.savefig("problem_8a.pdf")
 #plt.clf()
 
@@ -25,7 +25,7 @@ plt.show()
 for n in [10, 100, 1000, 10000]:
     exact = np.loadtxt(f"exact_solution_n_{n}.txt", delimiter = ", ", usecols = range(2)) 
     numeric = np.loadtxt(f"numeric_solution_n_{n}.txt", delimiter = ", ", usecols = range(2))
-    diff = np.abs(((exact[1:-1,1] - numeric[1:-1,1])/exact[1:-1,1]))
+    diff = np.log10(np.abs(((exact[1:-1,1] - numeric[1:-1,1]) / exact[1:-1,1])))
     #max_rel.append(np.max(10**diff))
     plt.plot(exact[1:-1,0], diff, label = f"N = {n}")
 
